719a7ca8667db98d1663b8ad50569e8196ab757d
Felipe Parra <fpr29.10@gmail.com>
2025-01-29 08:58:01 -0600

    🔒 feat(layout): Enhance application layout with routing and styling

--- src/app/(application)/layout.tsx
+++ src/app/(application)/layout.tsx
@@ -1,9 +1,22 @@
 'use client';
+import '@/global.scss';
 
+import DefaultActionBar from '@/components/page/DefaultActionBar';
 import { useSession } from '@/lib/auth-client';
+import { redirect, useRouter } from 'next/navigation';
 
 export default function ApplicationLayout({ children }: { children: React.ReactNode }) {
   const { data } = useSession();
+  const router = useRouter();
 
-  return <>{children}</>;
+  if (data && data.user) {
+    router.push('/dashboard');
+  }
+
+  return (
+    <main className="flex flex-col items-center justify-center h-screen relative py-8">
+      <DefaultActionBar />
+      {children}
+    </main>
+  );
 }
